42374b303034b6a1770a3a86be86cab3c3b77a6b
aamp19 <51056791+aamp19@users.noreply.github.com>
2021-08-09 19:42:29 -0400

    Update prescription-patientID.js

--- src/components/prescription-patientID.js
+++ src/components/prescription-patientID.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 let patientId = Math.floor(Math.random() * 10)
 async function apiRequest(e) {
     e.preventDefault()
-    let res = await axios.get(`http://kennedy-dev1.gojitech.systems/api/v1/oscarrest/prescription/${patientId}`, { "patientId": `14` })
+    let res = await axios.get(`https://kennedy-dev1.gojitech.systems/api/v1/oscarrest/prescription/${patientId}`, { "patientId": `14` })
     console.log(res)
 }
 
