f92f06b1951d05da85bfd47fed4f04d2fc2a1122
aamp19 <51056791+aamp19@users.noreply.github.com>
2021-08-09 19:37:38 -0400

    Update OscarpateintsAll.js

--- src/components/OscarpateintsAll.js
+++ src/components/OscarpateintsAll.js
@@ -6,7 +6,7 @@ const OscarpateintsAll = () => {
     const [error, setError] = useState('')
     async function apiRequest(e) {
         e.preventDefault()
-        axios.get(`http://kennedy-dev1.gojitech.systems/api/v1/oscar/patients/all`)
+        axios.get(`https://kennedy-dev1.gojitech.systems/api/v1/oscar/patients/all`)
             .then((res) => {
                 console.log(res.status)
                 if (res.status === 200) {
