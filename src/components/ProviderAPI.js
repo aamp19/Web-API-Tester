0e55f43482eaef9b439c48ae68bc2f9020c19a83
aamp19 <51056791+aamp19@users.noreply.github.com>
2021-08-17 10:56:36 -0400

    Update ProviderAPI.js

--- src/components/ProviderAPI.js
+++ src/components/ProviderAPI.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
-const ProviderAPI = () => {
+const ProviderAPI = (props) => {
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState()
     const [error, setError] = useState('')
-    async function apiRequest(e) {
-        e.preventDefault()
+    props.ProviderCallback(apiRequest)
+    async function apiRequest() {
+        // e.preventDefault()
         axios.get(`https://kennedy-dev1.gojitech.systems/api/v1/oscarrest/providers`)
             .then((res) => {
                 console.log(res.status)
@@ -24,23 +25,23 @@ const ProviderAPI = () => {
 
     return (
         <div>
-            <form >
-                <table>
-                    <tr>
-                        <td><b><span>GET:   </span> /api/v1/oscarrest/providers</b></td>
-                        <td><button id="oscarrest-providers" onClick={apiRequest}>Test</button></td>
-                    </tr>
-                </table>
-                {data ? data.map(e =>
+            {/* <form > */}
+            <table>
+                <tr>
+                    <td><b><span>GET:   </span> /api/v1/oscarrest/providers</b></td>
+                    <td><button id="oscarrest-providers" onClick={apiRequest}>Test</button></td>
+                </tr>
+            </table>
+            {data ? [data].map(e =>
 
-                    <p>{JSON.stringify(e)}</p>
-                    // <p>hello world</p>
+                <p>{JSON.stringify(e)}</p>
+                // <p>hello world</p>
 
-                ) : <div></div>}
-                {/* <p>{data}</p> */}
-                {error && JSON.stringify(error)}
+            ) : <div></div>}
+            {/* <p>{data}</p> */}
+            {error && JSON.stringify(error)}
 
-            </form>
+            {/* </form> */}
         </div>
     )
 }
