1a602dde28f3145ee34ac4744202ce3aa0f78991
aamp19 <51056791+aamp19@users.noreply.github.com>
2021-08-17 10:54:20 -0400

    Update OscarpatientsLabresults.js

--- src/components/OscarpatientsLabresults.js
+++ src/components/OscarpatientsLabresults.js
@@ -1,1 +1,42 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const OscarpatientsLabresults = (props) => {
+    const [data, setData] = useState()
+    const [error, setError] = useState('')
+    props.OscarPatientsLabCallback(apiRequest)
+    async function apiRequest() {
+        axios.get(`https://kennedy-dev1.gojitech.systems/api/v1/oscar/patients/14/labResults?skip=2&take=3`)
+            .then((res) => {
+                console.log(res.status)
+                if (res.status === 200) {
+                    console.log(res.data)
+                    setData(res.data)
+                }
+            })
+            .catch(err => {
+                setError(err)
+            })
+    }
+    return (
+        <div>
+            <table>
+                <tr>
+                    <td><b><span>GET:   </span> /api/v1/oscar/patients/14/labResults</b></td>
+                    <td><button id="oscarpatient" onClick={apiRequest}>Test</button></td>
+                </tr>
+            </table>
+            {data ? [data].map(e =>
+
+                <p>{JSON.stringify(e)}</p>
+                // <p>hello world</p>
+
+            ) : <div></div>}
+            {/* <p>{data}</p> */}
+            {error && JSON.stringify(error)}
+        </div>
+    )
+}
+
+export default OscarpatientsLabresults
 
