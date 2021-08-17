ca2dbe11546b1ff55b5274558b297ebc277ca648
aamp19 <51056791+aamp19@users.noreply.github.com>
2021-08-17 10:52:45 -0400

    Update OscarpatientsDocuments.js

--- src/components/OscarpatientsDocuments.js
+++ src/components/OscarpatientsDocuments.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
-const OscarpatientsDocuments = () => {
+const OscarpatientsDocuments = (props) => {
     const [data, setData] = useState()
     const [error, setError] = useState('')
-    async function apiRequest(e) {
-        e.preventDefault()
+    props.OscarPatientsDocCallback(apiRequest)
+    async function apiRequest() {
         axios.get(`https://kennedy-dev1.gojitech.systems/api/v1/oscar/patients/14/documents`)
             .then((res) => {
                 console.log(res.status)
@@ -20,22 +20,22 @@ const OscarpatientsDocuments = () => {
     }
     return (
         <div>
-            <form>
-                <table>
-                    <tr>
-                        <td><b><span>GET:   </span> /api/v1/oscar/patients/14/documents</b></td>
-                        <td><button id="oscarpatient" onClick={apiRequest}>Test</button></td>
-                    </tr>
-                </table>
-                {data ? [data].map(e =>
+            {/* <form> */}
+            <table>
+                <tr>
+                    <td><b><span>GET:   </span> /api/v1/oscar/patients/14/documents</b></td>
+                    <td><button id="oscarpatient" onClick={apiRequest}>Test</button></td>
+                </tr>
+            </table>
+            {data ? [data].map(e =>
 
-                    <p>{JSON.stringify(e)}</p>
-                    // <p>hello world</p>
+                <p>{JSON.stringify(e)}</p>
+                // <p>hello world</p>
 
-                ) : <div></div>}
-                {/* <p>{data}</p> */}
-                {error && JSON.stringify(error)}
-            </form>
+            ) : <div></div>}
+            {/* <p>{data}</p> */}
+            {error && JSON.stringify(error)}
+            {/* </form> */}
         </div>
     )
 }
