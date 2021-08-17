ce0ae856da2ee0c5ff84e1ed9c4e895cc8404159
aamp19 <51056791+aamp19@users.noreply.github.com>
2021-08-17 10:57:30 -0400

    Update notes-patientID.js

--- src/components/notes-patientID.js
+++ src/components/notes-patientID.js
@@ -3,12 +3,13 @@ import axios from 'axios'
 // let patientId = Math.floor(Math.random() * 10) + 1
 
 
-const NotesPatientID = () => {
+const NotesPatientID = (props) => {
 
     const [data, setData] = useState()
     const [error, setError] = useState('')
-    async function apiRequest(e) {
-        e.preventDefault()
+    props.notes_patientIDCallback(apiRequest)
+    async function apiRequest() {
+        // e.preventDefault()
         await axios.get(`https://kennedy-dev1.gojitech.systems/api/v1/oscarrest/notes/10`)
             .then((res) => {
                 console.log(res.status)
@@ -25,22 +26,22 @@ const NotesPatientID = () => {
 
     return (
         <div>
-            <form>
-                <table>
-                    <tr>
-                        <td><b><span>GET:   </span> /api/v1/oscarrest/notes/10</b></td>
-                        <td><button id="oscarrest-notes-patientID" onClick={apiRequest}>Test</button></td>
-                    </tr>
-                </table>
-                {data != [] ? [data].map(e =>
+            {/* <form> */}
+            <table>
+                <tr>
+                    <td><b><span>GET:   </span> /api/v1/oscarrest/notes/10</b></td>
+                    <td><button id="oscarrest-notes-patientID" onClick={apiRequest}>Test</button></td>
+                </tr>
+            </table>
+            {data != [] ? [data].map(e =>
 
-                    <p>{JSON.stringify(e)}</p>
-                    // <p>hello world</p>
+                <p>{JSON.stringify(e)}</p>
+                // <p>hello world</p>
 
-                ) : <div></div>}
-                {/* <p>{data}</p> */}
-                {error && JSON.stringify(error)}
-            </form>
+            ) : <div></div>}
+            {/* <p>{data}</p> */}
+            {error && JSON.stringify(error)}
+            {/* </form> */}
         </div>
     )
 }
