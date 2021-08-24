b70a46299448e4ab359f602afc7a04dba362e782
aamp19 <51056791+aamp19@users.noreply.github.com>
2021-08-24 11:18:37 -0400

    Update OscarpatientsDocuments.js

--- src/components/OscarpatientsDocuments.js
+++ src/components/OscarpatientsDocuments.js
@@ -29,7 +29,7 @@ const OscarpatientsDocuments = (props) => {
             </table>
             {data ? [data].map(e =>
 
-                <p>{JSON.stringify(e)}</p>
+                <p className="success">{JSON.stringify(e)}</p>
                 // <p>hello world</p>
 
             ) : <div></div>}
